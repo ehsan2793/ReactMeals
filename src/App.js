383210ebb2187e5c0feed77c486ebe87c2d06dff
ehsan2793 <ehsan2793@gmail.com>
2022-02-27 18:36:41 -0800

    portalElement added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meal from './components/Meals/Meal';
 function App() {
   return (
     <React.Fragment>
+      <Cart />
       <Header />
       <main>
         <Meal />
