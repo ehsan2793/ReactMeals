983043e72afbdbb52c8ae6896e28651338445a6a
ehsan2793 <ehsan2793@gmail.com>
2022-02-27 16:31:01 -0800

    meal summary added

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import mealsImage from '../../assets/meals.jpg'
-import classes from './Header.module.css'
+import React from 'react';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 const Header = (props) => {
     return (
         <React.Fragment>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImage} alt="Table full of diffrent foods" />
             </div>
-
         </React.Fragment>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
