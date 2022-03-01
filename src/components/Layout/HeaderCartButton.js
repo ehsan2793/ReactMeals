ad199ff7a640beaf9a8618197ce9624539cb08cb
ehsan2793 <ehsan2793@gmail.com>
2022-02-28 20:22:10 -0800

    bump added

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,33 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 const HeaderCartButton = (props) => {
     const cartCtx = useContext(CartContext);
+    const [btnstate, setButtonState] = useState(false);
+    const { items } = cartCtx;
 
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
+
+    const btnClassess = `${classes.button} ${btnstate ? classes.bump : ''} `;
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setButtonState(true);
+        const timer = setTimeout(() => {
+            setButtonState(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClassess} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
